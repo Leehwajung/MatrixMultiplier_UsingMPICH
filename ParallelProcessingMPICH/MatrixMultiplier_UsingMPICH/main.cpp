b72b95b346e2e197696f71d8b0d983b1308b0951
이화중 <hj_tl22@live.co.kr>
2016-05-22 18:44:44 +0900

    Matrix Multiplication

--- ParallelProcessingMPICH/MatrixMultiplier_UsingMPICH/main.cpp
+++ ParallelProcessingMPICH/MatrixMultiplier_UsingMPICH/main.cpp
@@ -3,14 +3,22 @@
 
 #include "stdafx.h"
 
+#include "Matrix.cpp"
+
 #define HOST		(Rank)0		// Host Process (Rank 0)
 #define MAX_DATA	100
 
+#define WIDTH	3
+
 typedef int	Rank;				// Process Number
+
+using namespace std;
+using namespace MatrixSpace;
 
 enum Message
 {
-	multiply
+	total,
+	submul
 };
 
 void main(int argc, char **argv)
@@ -20,7 +28,7 @@ void main(int argc, char **argv)
 	char processor_name[MPI_MAX_PROCESSOR_NAME];
 	int namelen;
 
-	char buff[MAX_DATA];
+	//char buff[MAX_DATA];
 
 	MPI_Status status;
 	MPI_Init(&argc, &argv);
@@ -29,24 +37,68 @@ void main(int argc, char **argv)
 	MPI_Get_processor_name(processor_name, &namelen);
 
 	/* 여기까지 모든 프로그램이 동일하게 수행된다.
-	* rank 는 각 프로세스를 구별하기 위해 사용된다.
-	* 주로 rank 0 은 특별한 용도로 사용된다.
-	*/
+	 * rank 는 각 프로세스를 구별하기 위해 사용된다.
+	 * 주로 rank 0 은 특별한 용도로 사용된다.
+	 */
+
+	cout << "### Process " << rank << " from " << processor_name << " ###" << endl;
+	fflush(stdout);
+
+	Matrix<float> A(WIDTH, WIDTH);
+	Matrix<float> B(WIDTH, WIDTH);
+	Matrix<float> C(WIDTH, WIDTH);
+
+	// 임의 값 초기화
+	for (int i = 0; i < WIDTH; i++) {
+		for (int j = 0; j < WIDTH; j++) {
+			A[i][j] = (float)i;
+			B[i][j] = (float)j;
+		}
+	}
+
+	int divSize = A.getCapacity() / size;
+	int pos = rank * divSize;
+	int posY = pos / WIDTH;
+	int nextRankPos = (rank + 1) * divSize;
+	int nextRankPosY = nextRankPos / WIDTH;
+	int x = pos - posY * WIDTH;
+
+	for (int y = posY, p = pos; y < nextRankPosY; y++) {
+		for (p, x; p < nextRankPos && x < C.getWidth(); p++, x++) {
+			// Csub is used to store the element
+			float Csub = 0;
+
+			// Loop over all the sub-matrices of A and B
+			for (int i = 0; i < WIDTH; i++) {
+				Csub += A[y][i] * B[i][x];
+			}
+			C[y][x] = Csub;
+		}
+		x = 0;
+	}
 
 	/* 이 부분부터 프로세스의 rank 에 따라 수행할 코드가 달라진다. */
 	if (rank == HOST) {	// rank 가 0 번인 프로세스가 수행할 코드
-		printf_s("### Process %d from %s ###\n", rank, processor_name);
-		fflush(stdout);
 		for (Rank i = 1; i < size; i++) {
-			MPI_Recv(buff, MAX_DATA, MPI_CHAR, i, multiply, MPI_COMM_WORLD, &status);
-			printf_s("%s\n", buff);
-			fflush(stdout);
+			for (int p = rank * divSize; p < (rank + 1) * divSize; p++) {
+				float value;
+				MPI_Recv(&value, sizeof(float), MPI_FLOAT, i, p, MPI_COMM_WORLD, &status);
+				C.getData()[p] = value;
+			}
+		}
+
+		cout << endl;
+		for (int i = 0; i < WIDTH; i++) {
+			for (int j = 0; j < WIDTH; j++) {
+				cout << C[i][j] << " ";
+			}
+			cout << endl;
 		}
 	}
 	else {				// rank 가 0 이 아닌 프로세스가 수행할 코드
-		sprintf_s(buff, MAX_DATA, "### Process %d from %s ###", rank, processor_name);
-		fflush(stdout);
-		MPI_Send(buff, MAX_DATA, MPI_CHAR, HOST, multiply, MPI_COMM_WORLD);
+		for (int p = rank * divSize; p < (rank + 1) * divSize; p++) {
+			MPI_Send(&C.getData()[p], sizeof(float), MPI_FLOAT, HOST, p, MPI_COMM_WORLD);
+		}
 	}
 
 	MPI_Finalize();
--- ParallelProcessingMPICH/MatrixMultiplier_UsingMPICH/stdafx.h
+++ ParallelProcessingMPICH/MatrixMultiplier_UsingMPICH/stdafx.h
@@ -14,6 +14,7 @@
 
 // TODO: 프로그램에 필요한 추가 헤더는 여기에서 참조합니다.
 
+#include <iostream>
 #include "mpi.h"
 #include "Matrix.h"
 #include "MatrixMul.h"
