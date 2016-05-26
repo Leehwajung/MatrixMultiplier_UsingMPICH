87c5835c754f31a6123d8089a526ca0f16b3b01e
songmho <songmho@gmail.com>
2016-05-26 19:08:19 +0900

    MPI 사용하여 코드 수정(1대의 경우 잘 돌아감)

--- ParallelProcessingMPICH/MPICH2_Tester/test.c
+++ ParallelProcessingMPICH/MPICH2_Tester/test.c
@@ -7,9 +7,7 @@
 #include <time.h>
 #include <windows.h>
 
-#define MAX_DATA 100
-
-# define arr_len 100	//배열의 길이 
+# define arr_len 250	//배열의 길이 
 
 int main(int argc, char **argv)
 {
@@ -17,7 +15,7 @@ int main(int argc, char **argv)
 	int size;
 	char processor_name[MPI_MAX_PROCESSOR_NAME];
 	int namelen;
-	char buff[MAX_DATA];
+	char buff[arr_len];
 	int i;
 
 	int arr_1[arr_len][arr_len];	// 배열 1
@@ -44,22 +42,70 @@ int main(int argc, char **argv)
 
 	QueryPerformanceFrequency(&tFreq);        // 주파수 측정
 	QueryPerformanceCounter(&tStart);            // 카운트 시작
-	for (int k = 0; k<arr_len; k++){				//결과 배열 y값 반복문
-		for (int i = 0; i<arr_len; i++){		//결과배열 x값(배열 1 y, 배열 2 x) 반복문 
-			int buf = 0;	//배열 곱셈 시, 곱셈과 덧셈하는 부분 저장하는 변수
 
-			for (int j = 0; j<arr_len; j++)		// 배열 1 x, 배열 2 y값 반복문
-				buf = buf + (arr_1[i][j] * arr_2[j][i]);		//배열 1줄 곱셈연산
-			result[k][i] = buf;	//1줄 곱셈한 결과 결과 배열에 저장
+	if (rank == 0){		//프로세스 0번일 때 
+
+
+
+		for (i = 1; i < size; i++) {		//배열 전송
+			MPI_Send(arr_1, arr_len*arr_len, MPI_INT, i, 0, MPI_COMM_WORLD);
+			MPI_Send(arr_2, arr_len*arr_len, MPI_INT, i, 1, MPI_COMM_WORLD);
 		}
-	}
 
-	QueryPerformanceCounter(&tEnd);            // 카운트 종료
-	tElapsedTime = ((tEnd.QuadPart - tStart.QuadPart) / (float)tFreq.QuadPart);
-	// 측정 시간 저장
-	printf("\n multiplication ended.\n");
-	printf("measure time : %lf s", tElapsedTime);
+
+		for (int k = 0; k < arr_len; k++){				//결과 배열 y값 반복문
+			for (int i = 0; i < arr_len; i++){		//결과배열 x값(배열 1 y, 배열 2 x) 반복문 
+				if (k%size == rank){
+					int buf = 0;
+					for (int j = 0; j < arr_len; j++)
+						buf = buf + (arr_1[i][j] * arr_2[j][i]);		//배열 1줄 곱셈연산
+					result[k][i] = buf;
+
+				}
+
+			}
+
+		}
+
+		for (int k = 0; k < arr_len; k++)
+			if (k%size != 0){
+				for (int i = 0; i < arr_len; i++){
+					MPI_Recv(buff, arr_len, MPI_INT, 0, k, MPI_COMM_WORLD, &status);
+					result[k][i] = buff[i];
+				}
+			}
+
+
+		QueryPerformanceCounter(&tEnd);            // 카운트 종료
+		tElapsedTime = ((tEnd.QuadPart - tStart.QuadPart) / (float)tFreq.QuadPart);
+		// 측정 시간 저장
+		printf("\n multiplication ended.\n");
+		printf("measure time : %lf s", tElapsedTime);
+	}		//end if
 	
+	else{		//프로세서가 0이 아닐 경우
+		for (i = 1; i < size; i++) {		//프로세서0에서 전송한 배열 받음
+			MPI_Recv(arr_1, arr_len*arr_len, MPI_INT, i, 0, MPI_COMM_WORLD, &status);
+			MPI_Recv(arr_2, arr_len*arr_len, MPI_INT, i, 1, MPI_COMM_WORLD, &status);
+
+		}
+		for (int k = 0; k<arr_len; k++){				//결과 배열 y값 반복문
+			for (int i = 0; i<arr_len; i++){		//결과배열 x값(배열 1 y, 배열 2 x) 반복문 
+				if (k%size == 0){
+					buff[i] = 0;	//배열 곱셈 시, 곱셈과 덧셈하는 부분 저장하는 변수
+					for (int j = 0; j < arr_len;j++)
+						buff[i] = buff[i] + (arr_1[i][j] * arr_2[j][i]);		//배열 1줄 곱셈연산
+
+
+				}
+								
+			}
+			MPI_Send(buff, arr_len, MPI_INT, 0, k, MPI_COMM_WORLD);		//프로세서 0에 result 전송
+
+		}
+	
+	}		//end else
+
 	
 	/*
 	if (rank == 0) {
